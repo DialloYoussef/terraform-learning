76040cf82768ad0f62519ca52e4af57cc52b395c
DialloYoussef <dialloyoussoufubo@gmail.com>
2026-07-31 23:54:20 +0000

    security group et vpc...

--- 03-aws/main.tf
+++ 03-aws/main.tf
@@ -6,3 +6,27 @@ resource "aws_s3_bucket" "learning" {
     Env  = "formation"
   }
 }
+
+resource "aws_security_group" "learning_sg" {
+  name        = "tf-learning-sg"
+  description = "Security group pour le module d'apprentissage"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf-learning-sg"
+  }
+}
