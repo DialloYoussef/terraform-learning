a7ce25c0f31692531b882bbedbf6962fd70e97ec
DialloYoussef <dialloyoussoufubo@gmail.com>
2026-07-31 22:27:40 +0000

    utilisation du count....

--- 02-aller-plus-loin-en-local/main.tf
+++ 02-aller-plus-loin-en-local/main.tf
@@ -8,7 +8,12 @@ terraform {
 }
 
 resource "local_file" "note" {
-  count    = 3
-  filename = "${path.module}/note-${count.index}.txt"
-  content  = count.index == 1 ? "CONTENU MODIFIÉ" : "Ceci est la note numéro ${count.index}"
+  for_each = {
+    intro    = "Ceci est l'introduction"
+    concepts = "Ceci contient les concepts clés"
+    exercice = "Ceci contient l'exercice pratique"
+  }
+
+  filename = "${path.module}/note-${each.key}.txt"
+  content  = each.value
 }
