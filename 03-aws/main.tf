b3a016eedb4b5920a1b1a59d03b757a3c7326c0d
DialloYoussef <dialloyoussoufubo@gmail.com>
2026-08-01 00:15:55 +0000

    config d'un EC2

--- 03-aws/main.tf
+++ 03-aws/main.tf
@@ -47,3 +47,30 @@ resource "local_file" "private_key" {
   file_permission = "0600"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "learning_server" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.learning_sg.id]
+  key_name               = aws_key_pair.learning_key.key_name
+
+  tags = {
+    Name = "tf-learning-server"
+  }
+}
+
