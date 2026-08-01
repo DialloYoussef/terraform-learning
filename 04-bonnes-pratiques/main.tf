2a4ff9ffdf0e7d58e775a19c3942d69195414c53
DialloYoussef <dialloyoussoufubo@gmail.com>
2026-08-01 00:54:15 +0000

    utilisation des modules...

--- 04-bonnes-pratiques/main.tf
+++ 04-bonnes-pratiques/main.tf
@@ -11,3 +11,15 @@ resource "local_file" "config" {
   filename = "${path.module}/config.txt"
   content  = "db_password=${var.db_password}"
 }
+
+module "note_bienvenue" {
+  source      = "./modules/fichier-texte"
+  nom_fichier = "bienvenue.txt"
+  contenu     = "Bienvenue dans les modules Terraform !"
+}
+
+module "note_recap" {
+  source      = "./modules/fichier-texte"
+  nom_fichier = "recap.txt"
+  contenu     = "Ceci est généré par le même module, appelé une deuxième fois avec des valeurs différentes."
+}
